454219423187d949d15771de900c957b5c6e38c0
Dmitry Baklanov <n79613928275@gmail.com>
2021-06-24 11:50:32 +0400

    English fields for Article model

--- models/Article.js
+++ models/Article.js
@@ -6,10 +6,28 @@ const ArticleSchema = new mongoose.Schema(
     title: {
       type: String,
       maxLength: [100, 'Максимальная длина заголовка - 60 знаков'],
+      required: [true, 'Требуется добавить заголовок статьи'],
+    },
+    title_en: {
+      type: String,
+      maxLength: [100, 'Максимальная длина заголовка - 60 знаков'],
+      required: [true, 'Требуется добавить заголовок статьи на английском'],
     },
     text: {
       type: String,
       maxLength: [1000, 'Максимальная длина статьи - 1000 знаков'],
+      required: [true, 'Требуется добавить текст статьи '],
+    },
+    text_en: {
+      type: String,
+      maxLength: [1000, 'Максимальная длина статьи - 1000 знаков'],
+      required: [true, 'Требуется добавить текст на английском'],
+    },
+    keywords: {
+      type: String,
+    },
+    keywords_en: {
+      type: String,
     },
     image: {
       type: String,
